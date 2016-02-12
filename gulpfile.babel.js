2e219b12e562bdaf9a497d3521a74cea2898d979
burczu <b.dybowski@gmail.com>
2016-02-12 16:00:37 +0100

    eslint configured

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,11 +3,22 @@ import rename from 'gulp-rename';
 import path from 'path';
 import babel from 'gulp-babel';
 import uglify from 'gulp-uglify';
+import eslint from 'gulp-eslint';
 
-gulp.task('default', () => {
+gulp.task('default', [ 'eslint', 'watch' ], () => {
   return gulp.src(path.join('src', 'index.js'))
     .pipe(babel())
     .pipe(uglify())
     .pipe(rename('react-preloader.js'))
     .pipe(gulp.dest('standalone'));
+});
+
+gulp.task('watch', () => {
+  gulp.watch('src/*.js', [ 'eslint' ]);
+});
+
+gulp.task('eslint', () => {
+  return gulp.src(['src/*.js'])
+    .pipe(eslint())
+    .pipe(eslint.format());
 });
